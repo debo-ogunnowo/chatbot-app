316887a44d4605842da32f0cf203fc27078d7611
Debo <deboogunnowo@gmail.com>
2025-12-17 17:06:57 +0100

    minor corrections

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'chaatbot.chatbot.urls'
+ROOT_URLCONF = 'chatbot.chatbot.urls'
 
 TEMPLATES = [
     {
